d5412797188f2050080f7cf89c9233110380a271
João Gabriel Medeiros Caldas <joao.caldas@creatorsfy.com>
2026-03-03 05:39:39 -0300

    fix: remover-css-desnecessario

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './Routes'; 
 
